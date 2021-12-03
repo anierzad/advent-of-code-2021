83539ef82394bf065b1d9a8fe9d2ca294c469f35
Adam Nierzad <adam.nierzad@awaze.co.uk>
2021-12-03 13:04:51 +0000

    Solved day 03 part two.

--- 03/main.go
+++ 03/main.go
@@ -19,18 +19,10 @@ func (dr DiagnosticReport) GammaRate() int {
 	for i := 0; i < 12; i++ {
 
 		posRune := '1'
-		zeros := 0
+		zeros := bitCount(dr.lines, i)
 
-		for _, line := range dr.lines {
-
-			if line[i] == '0' {
-				zeros++
-
-				if zeros > (len(dr.lines) / 2) {
-					posRune = '0'
-					break
-				}
-			}
+		if zeros > (len(dr.lines) / 2) {
+			posRune = '0'
 		}
 
 		gamma = gamma + string(posRune)
@@ -52,18 +44,10 @@ func (dr DiagnosticReport) EpsilonRate() int {
 	for i := 0; i < 12; i++ {
 
 		posRune := '1'
-		zeros := len(dr.lines)
+		zeros := bitCount(dr.lines, i)
 
-		for _, line := range dr.lines {
-
-			if line[i] == '1' {
-				zeros--
-
-				if zeros < (len(dr.lines) / 2) {
-					posRune = '0'
-					break
-				}
-			}
+		if zeros < (len(dr.lines) / 2) {
+			posRune = '0'
 		}
 
 		epsilon = epsilon + string(posRune)
@@ -86,11 +70,105 @@ func (dr DiagnosticReport) PowerConsumption() int {
 	return ga * ep
 }
 
+func (dr DiagnosticReport) OxygenGeneratorRating() int {
+
+	filtered := dr.lines
+
+	for i := 0; i < 12; i++ {
+
+		if len(filtered) < 2 {
+			break
+		}
+
+		tb := '1'
+
+		if bitCount(filtered, i) > (len(filtered) / 2) {
+			tb = '0'
+		}
+
+		filtered = bitFilter(filtered, i, tb)
+	}
+
+	// Convert to integer.
+	iv, err := strconv.ParseInt(filtered[0], 2, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return int(iv)
+}
+
+func (dr DiagnosticReport) CO2ScrubberRating() int {
+
+	filtered := dr.lines
+
+	for i := 0; i < 12; i++ {
+
+		if len(filtered) < 2 {
+			break
+		}
+
+		tb := '1'
+
+		if bitCount(filtered, i) <= (len(filtered) / 2) {
+			tb = '0'
+		}
+
+		filtered = bitFilter(filtered, i, tb)
+	}
+
+	// Convert to integer.
+	iv, err := strconv.ParseInt(filtered[0], 2, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return int(iv)
+}
+
+func (dr DiagnosticReport) LifeSupportRating() int {
+
+	og := dr.OxygenGeneratorRating()
+	cs := dr.CO2ScrubberRating()
+
+	return og * cs
+}
+
 func (dr DiagnosticReport) Print() {
 
-	fmt.Printf("Gamma Rate:..........%d\n", dr.GammaRate())
-	fmt.Printf("Epsilon Rate:........%d\n", dr.EpsilonRate())
-	fmt.Printf("Power Consumption:...%d\n", dr.PowerConsumption())
+	fmt.Printf("Gamma Rate:................%d\n", dr.GammaRate())
+	fmt.Printf("Epsilon Rate:..............%d\n", dr.EpsilonRate())
+	fmt.Printf("Power Consumption:.........%d\n", dr.PowerConsumption())
+	fmt.Printf("Oxygen Generator Rating:...%d\n", dr.OxygenGeneratorRating())
+	fmt.Printf("CO2 Scrubber Rating:.......%d\n", dr.CO2ScrubberRating())
+	fmt.Printf("Life Support Rating:.......%d\n", dr.LifeSupportRating())
+}
+
+func bitCount(lines []string, bit int) int {
+
+	zeros := 0
+
+	for _, line := range lines {
+		if line[bit] == '0' {
+			zeros++
+		}
+	}
+
+	return zeros
+}
+
+func bitFilter(lines []string, bit int, tb rune) []string {
+
+	filtered := []string{}
+
+	for _, line := range lines {
+
+		if rune(line[bit]) == tb {
+			filtered = append(filtered, line)
+		}
+	}
+
+	return filtered
 }
 
 func main() {
