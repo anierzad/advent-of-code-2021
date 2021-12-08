48b6a1dc31922a147251a5e35b122bb4ff0e41fc
Adam Nierzad <adam.nierzad@awaze.co.uk>
2021-12-08 18:02:38 +0000

    Solved day 08 part two.

--- 08/main.go
+++ 08/main.go
@@ -5,33 +5,210 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-type DisplayDigit struct {
-	output string
+var definitions = map[int][]rune{
+	0: {'a','b','c','e','f','g'},
+	1: {'c','f'},
+	2: {'a','c','d','e','g'},
+	3: {'a','c','d','f','g'},
+	4: {'b','c','d','f'},
+	5: {'a','b','d','f','g'},
+	6: {'a','b','d','e','f','g'},
+	7: {'a','c','f'},
+	8: {'a','b','c','d','e','f','g'},
+	9: {'a','b','c','d','f','g'},
 }
 
-func (dd DisplayDigit) UniqueSegment() bool {
-	if len(dd.output) == 2 ||
-		len(dd.output) == 3 ||
-		len(dd.output) == 4 ||
-		len(dd.output) == 7 {
-		return true
+type DisplayDigit struct {
+	output string
+	segments map[rune]bool
+}
+
+func (dd DisplayDigit) Value() int {
+	longest := 0
+	value := -1
+
+	for v, definition := range definitions {
+		match := true
+
+		for _, r := range definition {
+			if !dd.segments[r] {
+				match = false
+				break
+			}
+		}
+
+		if match {
+			length := len(definition)
+			if length > longest {
+				longest = len(definition)
+				value = v
+			}
+		}
 	}
 
-	return false
+	return value
 }
 
 func NewDisplayDigit(output string) *DisplayDigit {
-	return &DisplayDigit{
+	dd := &DisplayDigit{
 		output: output,
+		segments: make(map[rune]bool),
 	}
+
+	for _, r := range output {
+		dd.segments[r] = true
+	}
+
+	return dd
+}
+
+type Display struct {
+	digits []*DisplayDigit
+	signals []string
+}
+
+func (d Display) Value() int {
+	str := ""
+
+	for _, digit := range d.digits {
+		str = fmt.Sprintf("%s%d", str, digit.Value())
+	}
+
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		log.Fatal("Failed to convert string to int.")
+	}
+
+	return value
+}
+
+func NewDisplay(signals, outputs []string) *Display {
+
+	d := &Display{
+		digits:  make([]*DisplayDigit, 0),
+		signals: signals,
+	}
+
+	// We need to work out what segments map to which signals.
+	sigOne := stringWithLength(signals, len(definitions[1]))
+	sigFour := stringWithLength(signals, len(definitions[4]))
+	sigSeven := stringWithLength(signals, len(definitions[7]))
+	sigEight := stringWithLength(signals, len(definitions[8]))
+
+	// Make a mapping.
+	mapping := make(map[rune]rune)
+
+	// Process one.
+	for _, r := range sigOne {
+		count := runeOccourances(signals, r)
+
+		if count == 8 {
+			mapping[r] = 'c'
+		}
+
+		if count == 9 {
+			mapping[r] = 'f'
+		}
+	}
+
+	// Process four.
+	for _, r := range sigFour {
+
+		// Skip what we've already got.
+		if _, exists := mapping[r]; exists {
+			continue
+		}
+
+		count := runeOccourances(signals, r)
+
+		if count == 6 {
+			mapping[r] = 'b'
+		}
+
+		if count == 7 {
+			mapping[r] = 'd'
+		}
+	}
+
+	// Process seven.
+	for _, r := range sigSeven {
+
+		// Skip what we've already got.
+		if _, exists := mapping[r]; exists {
+			continue
+		}
+
+		count := runeOccourances(signals, r)
+
+		if count == 8 {
+			mapping[r] = 'a'
+		}
+	}
+
+	// Process eight.
+	for _, r := range sigEight {
+
+		// Skip what we've already got.
+		if _, exists := mapping[r]; exists {
+			continue
+		}
+
+		count := runeOccourances(signals, r)
+
+		if count == 4 {
+			mapping[r] = 'e'
+		}
+
+		if count == 7 {
+			mapping[r] = 'g'
+		}
+	}
+
+	// Create display.
+	for _, output := range outputs {
+
+		// Remap!
+		remapped := ""
+
+		for _, r := range output {
+			remapped = fmt.Sprintf("%s%s", remapped, string(mapping[r]))
+		}
+
+		digit := NewDisplayDigit(remapped)
+
+		d.digits = append(d.digits, digit)
+	}
+
+	return d
+}
+
+func runeOccourances(signals []string, r rune) (count int) {
+	for _, signal := range signals {
+		if strings.Contains(signal, string(r)) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func stringWithLength(strs []string, length int) string {
+	for _, s := range strs {
+		if len(s) == length {
+			return s
+		}
+	}
+
+	return ""
 }
 
 func main() {
 
-	digits := make([]*DisplayDigit, 0)
+	displays := make([]*Display, 0)
 
 	// Read our input file.
 	f, err := os.Open("./input")
@@ -52,23 +229,19 @@ func main() {
 		// Split display from inputs.
 		splitLine := strings.Split(line, " | ")
 
-		// Split display values.
-		splitDisplay := strings.Split(splitLine[1], " ")
+		signals := strings.Split(splitLine[0], " ")
+		outputs := strings.Split(splitLine[1], " ")
 
-		for _, display := range splitDisplay {
-			digit := NewDisplayDigit(display)
+		display := NewDisplay(signals, outputs)
 
-			digits = append(digits, digit)
-		}
+		displays = append(displays, display)
 	}
 
-	uniqueDigits := 0
+	total := 0
 
-	for _, digit := range digits {
-		if digit.UniqueSegment() {
-			uniqueDigits++
-		}
+	for _, display := range displays {
+		total += display.Value()
 	}
 
-	fmt.Println("Unique digits:", uniqueDigits)
+	fmt.Println("Total:", total)
 }
