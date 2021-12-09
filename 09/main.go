1d674f420a777d17ba765cc50448b76f39078a20
Adam Nierzad <adam.nierzad@awaze.co.uk>
2021-12-09 17:29:08 +0000

    Solved day 09 part two.

--- 09/main.go
+++ 09/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -34,7 +35,7 @@ func (pm PointMap) Risk() int {
 	risk := 0
 
 	for _, p := range pm.points {
-		if p.IsLow(pm.adjacentTo(p)) {
+		if p.IsLow(pm.points) {
 			risk += p.Risk()
 		}
 	}
@@ -42,42 +43,25 @@ func (pm PointMap) Risk() int {
 	return risk
 }
 
-func (pm PointMap) adjacentTo(p *Point) (adjPoints []*Point) {
+func (pm PointMap) BasinScore() int {
 
-	//adjPoints
-	adjacent := make([]Coordinate, 0)
+	scores := make([]int, 0)
 
-	// Get above.
-	if p.coordinate.y > 0 {
-		adjacent = append(adjacent, NewCoordinate(p.coordinate.x, (p.coordinate.y - 1)))
-	}
+	for _, p := range pm.points {
 
-	// Get Below.
-	if p.coordinate.y < pm.yMax {
-		adjacent = append(adjacent, NewCoordinate(p.coordinate.x, (p.coordinate.y + 1)))
-	}
-
-	// Get left.
-	if p.coordinate.x > 0 {
-		adjacent = append(adjacent, NewCoordinate((p.coordinate.x - 1), p.coordinate.y))
-	}
-
-	// Get Below.
-	if p.coordinate.x < pm.xMax {
-		adjacent = append(adjacent, NewCoordinate((p.coordinate.x + 1), p.coordinate.y))
-	}
-
-	// Get points at those coordinates.
-	for _, ac := range adjacent {
-		ap, exists := pm.points[ac]
-		if !exists {
-			fmt.Printf("Unable to find point at %+v\n", ac)
+		if p.IsLow(pm.points) {
+			scores = append(scores, p.BasinSize(pm.points))
 		}
-
-		adjPoints = append(adjPoints, ap)
 	}
 
-	return adjPoints
+	if len(scores) < 3 {
+		log.Fatal("Not enough basins!")
+	}
+
+	sort.Ints(scores)
+	total := scores[len(scores)-1] * scores[len(scores)-2] * scores[len(scores)-3]
+
+	return total
 }
 
 func NewPointMap() *PointMap {
@@ -91,19 +75,87 @@ type Point struct {
 	height int
 }
 
+func (p Point) adjacent(points map[Coordinate]*Point) (adjPoints []*Point) {
+
+	adjacent := []Coordinate{
+		NewCoordinate(p.coordinate.x, (p.coordinate.y - 1)),
+		NewCoordinate(p.coordinate.x, (p.coordinate.y + 1)),
+		NewCoordinate((p.coordinate.x - 1), p.coordinate.y),
+		NewCoordinate((p.coordinate.x + 1), p.coordinate.y),
+	}
+
+	// Get points at those coordinates.
+	for _, ac := range adjacent {
+		ap, exists := points[ac]
+		if !exists {
+			continue
+		}
+
+		adjPoints = append(adjPoints, ap)
+	}
+
+	return adjPoints
+}
+
 func (p Point) Risk() int {
 	return 1 + p.height
 }
 
-func (p Point) IsLow(points []*Point) bool {
+func (p Point) IsLow(points map[Coordinate]*Point) bool {
 
-	for _, ap := range points {
+	adjacent := p.adjacent(points)
+
+	for _, ap := range adjacent {
 		if ap.height <= p.height {
 			return false
 		}
 	}
 
 	return true
+}
+
+func (p *Point) BasinSize(points map[Coordinate]*Point) int {
+	all := p.identifyBasin(points, make([]*Point, 0))
+
+	return len(all)
+}
+
+func (p *Point) identifyBasin(points map[Coordinate]*Point, stack []*Point) []*Point {
+
+	// Heights of 9 or above don't count.
+	if p.height > 8 {
+		return stack
+	}
+
+	// Add myself to the stack.
+	stack = append(stack, p)
+
+	// Get adjacent.
+	adjacentPoints := p.adjacent(points)
+
+
+	for _, ap := range adjacentPoints {
+
+		// Skip if already in stack.
+		inStack := false
+
+		for _, sp := range stack {
+			if ap == sp {
+				inStack = true
+				break
+			}
+		}
+
+		if inStack {
+			continue
+		}
+
+		if ap.height > p.height {
+			stack = ap.identifyBasin(points, stack)
+		}
+	}
+
+	return stack
 }
 
 func NewPoint(x, y, height int) *Point {
@@ -157,4 +209,5 @@ func main() {
 	}
 
 	fmt.Println("Risk:", pointMap.Risk())
+	fmt.Println("Basin score:", pointMap.BasinScore())
 }
